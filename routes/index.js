6d25715843e8a15535f53fde0800dbfc08902cf5
Moon Jo <urwisdom77@gmail.com>
2020-05-20 17:08:16 -0400

    chore: change the index.js file path inside build for heroku

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const router = require("express").Router();
 
 router.use("/api", require("./api"));
 router.use((req, res) =>
-  res.sendFile(path.join(__dirname, "./client/index.html"))
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
 );
 module.exports = router;
