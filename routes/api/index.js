540a8f76d0fabb9615ba714dba23d4ac114c8f9c
Moon Jo <urwisdom77@gmail.com>
2020-05-20 17:51:50 -0400

    fix: add ping pong for checking if mLab working properly

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,9 @@ const path = require("path");
 const bookApi = require("./book");
 
 router.use("/books", bookApi);
+router.get("/ping", (req, res) => {
+  res.json("pong");
+});
 
 router.use(function (req, res) {
   res.sendFile(path.join(__dirname, "../../client/index.html"));
