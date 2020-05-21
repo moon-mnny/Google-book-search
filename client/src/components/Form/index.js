1c5ce9bc59ef77aa525fda3db62534baf4a4e02f
Moon Jo <urwisdom77@gmail.com>
2020-05-21 09:56:04 -0400

    chore: Change the placeholder for seach form

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -12,7 +12,7 @@ function Form({ q, handleInputChange, handleFormSubmit }) {
           id="Title"
           type="text"
           value={q}
-          placeholder="Ready Player One"
+          placeholder="What kind of books are you looking for?"
           name="q"
           onChange={handleInputChange}
           required
